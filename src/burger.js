162f545441c61dc060313ec7fcc7ac37ba96d7ce
34elo <evsenik23@mail.ru>
2025-01-26 17:34:30 +0300

    burger fix

--- src/burger.js
+++ src/burger.js
@@ -1,22 +1,24 @@
-const burgerBtn = document.querySelector('.header__hamburger');
-const navMenu = document.querySelector('.header__nav');
-const items = Array.from(document.querySelectorAll('.nav__item a'));
+if (innerWidth){
+  const burgerBtn = document.querySelector('.header__hamburger');
+  const navMenu = document.querySelector('.header__nav');
+  const items = Array.from(document.querySelectorAll('.nav__item a'));
 
-let isToogle = false;
+  let isToogle = false;
 
-items.forEach((item) => {
-  item.addEventListener('click', () => {
-    navMenu.style.display = 'none';
-    isToogle = false;
+  items.forEach((item) => {
+    item.addEventListener('click', () => {
+      navMenu.style.display = 'none';
+      isToogle = false;
+    });
   });
-});
 
-burgerBtn.addEventListener('click', () => {
-  if (isToogle) {
-    navMenu.style.display = 'none';
-    isToogle = false;
-  } else {
-    isToogle = true;
-    navMenu.style.display = 'block';
-  }
-});
+  burgerBtn.addEventListener('click', () => {
+    if (isToogle) {
+      navMenu.style.display = 'none';
+      isToogle = false;
+    } else {
+      isToogle = true;
+      navMenu.style.display = 'block';
+    }
+  });
+}
